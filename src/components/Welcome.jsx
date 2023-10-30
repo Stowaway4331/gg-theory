e419fe954c6ba59175b3d5ca664e54200f88e78c
Jeremie Dsouza <92860009+Stowaway4331@users.noreply.github.com>
2023-10-30 06:06:20 +0000

    refactored

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,13 +2,6 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {apiUrl} from '../Constants';
 
-// const apiKey = import.meta.env.VITE_QUIZ_API_KEY;
-// const limit = 10;
-// const category = 'Linux';
-// const difficulty = 'easy';
-
-// const apiUrl = `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=${limit}&category=${category}&difficulty=${difficulty}`;
-
 const Welcome = () => {
 
     const [data, setData] = useState([]);
@@ -22,8 +15,6 @@ const Welcome = () => {
         // console.log(newdata);
     }
 
-    // const getData = new Promise(getQuestions)
-
     useEffect(() => {
         getQuestions();
     }, [])
@@ -34,9 +25,8 @@ const Welcome = () => {
                 <h3 className="text-center mx-auto">Welcome to the ultimate Linux quiz!</h3>
                 {
                     data.length === 10 ? <button className="w-max mx-auto" onClick={() => {
-                        // setTriggerQuestions(!triggerQuestions);
                         navigate('/quiz', { state: { data: data } });
-                    }} >Click to Begin</button> : <p>Loading...</p>
+                    }} >Click to Begin</button> : <p className='w-max mx-auto'>Loading...</p>
                 }
             </div>
         </>
